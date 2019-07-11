f1203d2e90c7e612dc1714096b695a68a6199c5a
fpfrimer <31457238+fpfrimer@users.noreply.github.com>
2019-07-11 17:20:39 -0300

    Add files via upload

---
+++ tiva/lm35_average.ino
@@ -0,0 +1,42 @@
+
+#define SIZE 512
+#define PERIOD 100
+#define AVERAGE false
+
+long data[SIZE]; //Armazena os dados convertidos
+long acc;   //Acumulador
+unsigned int ctn_in = 0;  //Contador de entrada de dados
+unsigned int ctn_out = 1; //Contador de saída de dados
+float avg;                //Média dos valores lidos
+int ti,tf;
+
+
+void setup() {
+  
+  Serial.begin(9600); // inicia Serial
+  ti = millis();
+}
+
+
+void loop() {
+
+  if (AVERAGE == true){
+    data[ctn_in] = analogRead(A11);
+    acc += data[ctn_in++] - data[ctn_out++];
+    if (ctn_in == SIZE) ctn_in = 0;
+    if (ctn_out == SIZE) ctn_out = 0;
+    tf = millis();
+    if (tf - ti >= PERIOD){
+      avg = acc/SIZE;
+      Serial.println(avg*330/4095);
+      ti = millis();
+    }
+  }
+  else{
+    tf = millis();
+    if (tf - ti >= PERIOD){      
+      Serial.println((float)analogRead(A11)*330/4095);
+      ti = millis();
+    }
+  } 
+}
